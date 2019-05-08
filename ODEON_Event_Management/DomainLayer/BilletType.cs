a7122c47f2490c1ba4491cf072a5d55a4b8d9206
Christian T <chri466w@edu.eal.dk>
2019-05-08 08:19:46 +0200

    Stuff

--- ODEON_Event_Management/DomainLayer/BilletType.cs
+++ ODEON_Event_Management/DomainLayer/BilletType.cs
@@ -8,6 +8,7 @@ namespace DomainLayer
 {
     public class BilletType
     {
+        public int ID;
         public int Udbud;
         public decimal Pris;
         public List<SalgsTal> SamledeSalgsTal = new List<SalgsTal>();
