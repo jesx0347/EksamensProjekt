9664f44a7f8ae3d34dce74ebec36c9585386fbd1
michaelserupmj <michaelserupmj@gmail.com>
2019-05-09 09:29:50 +0200

    Forsøg på at lave test til IndskrivSal

--- ODEON_Event_Management/UnitTestProject1/UnitTestController.cs
+++ ODEON_Event_Management/UnitTestProject1/UnitTestController.cs
@@ -25,8 +25,6 @@ namespace UnitTestProject1
             control = new Controller(Salrepo, Katrepo, OErepo);
         }
 
-        //string name, IEnumerable<DateTime> dates
-
         [TestMethod]
         public void TestIndskrivNavnOgDato()
         {
@@ -45,6 +43,40 @@ namespace UnitTestProject1
                 Assert.AreEqual(dates[i], OErepo.GetItem(1).Afviklinger[i].Dato);
             }
         }
- 
+
+        [TestMethod]
+        public void TestIndskrivSal()
+        {
+            List<DateTime> dates = new List<DateTime>();
+            dates.Add(new DateTime(2019, 5, 8));
+            dates.Add(new DateTime(2019, 5, 28));
+            dates.Add(new DateTime(2019, 6, 15));
+            dates.Add(new DateTime(2019, 6, 20));
+
+            string name = "Nyt Event";
+            control.IndskrivNavnOgDato(name, dates);
+
+            //List<ODEONEvent> events = new List<ODEONEvent>();
+            //events.Add(new ODEONEvent("Nytårsfest", 1));
+
+            List<Sal> sale = new List<Sal>();
+            sale.Add(new Sal("Store Sal", 1, (decimal)50000.00, 1740));
+            Salrepo.AddItem(sale);
+
+            int IDEvent = OErepo.GetItem(1).ID;
+            string SalNavn = "Store Sal";
+
+            control.IndskrivSal(IDEvent, SalNavn);
+
+            Assert.AreEqual("Store Sal", Salrepo.GetItem(1).Navn);
+        }
+
+        //[TestMethod]
+        //public void TestIndskrivKategori()
+        //{
+        //    List<Kategori> kats = new List<Kategori>();
+        //    kats.Add(new Kategori(""))
+
+        //}
     }
 }
