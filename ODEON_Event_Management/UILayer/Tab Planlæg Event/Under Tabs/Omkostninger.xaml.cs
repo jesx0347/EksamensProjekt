9477d23b983612b9250a59e727d846febab80636
michaelserupmj <michaelserupmj@gmail.com>
2019-05-13 13:10:31 +0200

    Omkostninger har nu en Næste knap, som kalder på IndskrivOmkosninger.

--- ODEON_Event_Management/UILayer/Tab Planlæg Event/Under Tabs/Omkostninger.xaml.cs
+++ ODEON_Event_Management/UILayer/Tab Planlæg Event/Under Tabs/Omkostninger.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ApplicationLayer;
 
 namespace UILayer.Tab_Planlæg_Event.Under_Tabs
 {
@@ -23,6 +24,24 @@ namespace UILayer.Tab_Planlæg_Event.Under_Tabs
         public Omkostninger()
         {
             InitializeComponent();
+        }
+
+        private void Button_Omkostninger_Næste_Click(object sender, RoutedEventArgs e)
+        {
+            //decimal m;
+            if(decimal.TryParse(TextBox_Markedsføring.Text, out decimal m))
+            {
+                if(double.TryParse(TextBox_KODA.Text, out double k))
+                {
+                    if (decimal.TryParse(TextBox_Garantisum.Text, out decimal g))
+                    {
+                        if (double.TryParse(TextBox_ArtistSplit.Text, out double a))
+                        {
+                            Controller.Singleton.IndskrivOmkostninger(NavnOgDato.TempID, m, k, g, a);
+                        }
+                    }
+                }
+            }
         }
     }
 }
