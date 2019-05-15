289346f3c6c000681621ee83b61df008ad221d1e
michaelserupmj <michaelserupmj@gmail.com>
2019-05-15 08:30:25 +0200

    Tilføjet en "Tilføj Billet" knap til BilletType

--- ODEON_Event_Management/UILayer/Tab Planlæg Event/Under Tabs/BilletType.xaml.cs
+++ ODEON_Event_Management/UILayer/Tab Planlæg Event/Under Tabs/BilletType.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ApplicationLayer;
 
 namespace UILayer.Tab_Planlæg_Event.Under_Tabs
 {
@@ -20,9 +21,29 @@ namespace UILayer.Tab_Planlæg_Event.Under_Tabs
     /// </summary>
     public partial class BilletType : Page
     {
+        List<Tuple<int, decimal>> billet = new List<Tuple<int, decimal>>();
+        
+
         public BilletType()
         {
             InitializeComponent();
+
+        }
+
+        private TextBox CreateTextBox(int row, int column)
+        {
+            TextBox tb = new TextBox();
+            tb.Margin = new Thickness(5);
+            tb.Height = 22;
+            tb.Width = 150;
+            Grid.SetColumn(tb, column);
+            Grid.SetRow(tb, row);
+            return tb;
+        }
+
+        private void Button_tilføj_Billet_Click(object sender, RoutedEventArgs e)
+        {
+            CreateTextBox(3, 5);
         }
     }
 }
